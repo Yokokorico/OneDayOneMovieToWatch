3e76e739af2df27d59bb463a0643f97590c0e17f
yokokoricot <theo.duflos.yokoko@gmail.com>
2024-01-29 22:40:25 +0100

    feat: :sparkles: Create the  home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { AcceuilComponent } from './acceuil/acceuil.component'; 
 @NgModule({
   declarations: [
     AppComponent,
+    AcceuilComponent,
   ],
   imports: [
     BrowserModule,
